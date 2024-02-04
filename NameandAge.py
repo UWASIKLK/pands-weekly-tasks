6616de7f98840106156f5c2592f73d210adb690e
Katarina Siklodyova <hrobkova@hotmail.com>
2024-02-04 12:31:25 +0000

    week 2 tasks

---
+++ NameandAge.py
@@ -0,0 +1,16 @@
+#NameandAge.py
+#this will request your name and your age
+#program will print out both in on sentence
+
+'''
+name = input("What is your name?  ")  
+age = int( input("What is your age? "))
+
+print ('Hello {}, your age is {}.' .format(name,age))
+'''
+# modified program using \t
+
+name = input("What is your name?  ")  
+age = int( input("What is your age? "))
+
+print ('Hello {}, \tyour age is {}.' .format(name,age))
