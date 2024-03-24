dbadcf9cbfda5397495c8058f83c3386d215b24f
Katarina Siklodyova <hrobkova@hotmail.com>
2024-03-24 12:32:50 +0000

    update week08

--- week08/plottask.py
+++ week08/plottask.py
@@ -1,25 +1,25 @@
 #program will do a histogram of a normal distribution of a 1000 valuses, mean of 5 and standad deviation of 2
 #this will plot the function h(x)= x^3 in the rage of 0-10
 
-import numpy as np
+import numpy as np              # importing libraries to create a plot
 import matplotlib.pyplot as plt
 
 
-x_value = np.array(range(0,10))
-y_value = x_value ** 3
+x_value = np.array(range(0,10)) # setting up rage 0-10 for x-values
+y_value = x_value ** 3  #setting the y-values as x^3
 
-plt.plot(x_value, y_value, label = "h(x)=x^3", color = "red")
-med = 5
-dev = 2
-values = 1000
+plt.plot(x_value, y_value, label = "h(x)=x^3", color = "red") #setting the plot
+med = 5 #setting the mean
+dev = 2 #setting standard deviation
+values = 1000 #setting values
 
-np.random.seed(1)
+np.random.seed(1)                           #normal distribution with the seed set to 1
 numbers = np.random.normal(med, dev, values)
 
-plt.hist (numbers)
-plt.title("Function x^3")
-plt.xlabel("X values range")
-plt.ylabel("Output of (x=x^3) values")
-plt.grid()
-plt.legend()
-plt.show()
+plt.hist (numbers, color = "lime", ec ="red", lw=2)#creating plot using hist() function
+plt.title("Function x^3")                   #setting title name
+plt.xlabel("X values range")                #labeling x values
+plt.ylabel("Output of (x=x^3) values")      #labeling y values
+plt.grid()                                  #adding grids
+plt.legend()                                #displaying legend
+plt.show()                                  #displaing histogram
